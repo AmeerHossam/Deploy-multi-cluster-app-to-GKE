b6c8bdfb9847ef361313c79191ff42987effacc9
AmirHossam <amirhossameldien@gmail.com>
2024-04-07 15:32:08 +0200

    Adding Storage cluster

--- Infrastructure/provider.tf
+++ Infrastructure/provider.tf
@@ -4,20 +4,21 @@ provider "google" {
 }
 
 
-resource "google_storage_bucket" "terraform-bucket-for-state" {
-  name                        = "bucket-for-mystatefile"
-  location                    = "US"
-  public_access_prevention    = "enforced"
-  uniform_bucket_level_access = true
-  versioning {
-    enabled = true
-  }
-  labels = {
-    "environment" = "jorgebernhnardt"
-  }
-}
+# resource "google_storage_bucket" "terraform-bucket-for-state" {
+#   name                        = "bucket-for-mystatefile"
+#   location                    = "US"
+#   public_access_prevention    = "enforced"
+#   uniform_bucket_level_access = true
+#   versioning {
+#     enabled = true
+#   }
+#   labels = {
+#     "environment" = "jorgebernhnardt"
+#   }
+# }
 terraform {
-  backend "local" {
-    path = "./terraform.tfstate"
+  backend "gcs" {
+    bucket  = "state_bucket_terraform"
+    prefix  = "terraform/state"
   }
 }
